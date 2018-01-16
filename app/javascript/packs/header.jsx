66fcda12f4e0cf499da5db740baed715889b3334
Ian Horsman <ianphorsman@gmail.com>
2018-01-16 17:38:35 -0500

    Header and Footer components

---
+++ app/javascript/packs/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+
+    render() {
+        return (
+            <header className="fixed-top">
+                <div className="navbar">
+                    <h1>
+                        Clarifai App
+                    </h1>
+                </div>
+            </header>
+        )
+    }
+}
