c6caa71edb2c44a5b81b912556eb488f126ff016
Ian Horsman <ianphorsman@gmail.com>
2018-01-16 14:32:02 -0500

    Added clarifai, victory, and underscore to module for export

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const victory = require('victory')
 const _ = require('underscore')
-const clarifai = require('clarifai')
+const Clarifai = require('clarifai')
 
 module.exports = {
     environment,
     victory,
     _,
-    clarifai,
+    Clarifai,
 }
