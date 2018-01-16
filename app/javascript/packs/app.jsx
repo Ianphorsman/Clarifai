4738fe5c33998d217819f71a1c0184708dbce20b
Ian Horsman <ianphorsman@gmail.com>
2018-01-16 16:26:53 -0500

    Added Clarifai instance to parent react component state

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+const Clarifai = require('clarifai')
+
 export default class App extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            clarifai: new Clarifai.App({
+                apiKey: 'aea0318f9da24077a7fd41793bf609e2'
+            })
+        }
     }
 
     render() {
